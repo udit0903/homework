40835485a3cd761280e3131b3868bab73fbd0681
udit0903 <63818954+udit0903@users.noreply.github.com>
2020-08-06 22:56:05 +0530

    Updates

---
+++ precache-manifest.988dfeebc9d872711b1cad8efe7e0ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5362696e6faa02119412f6e69abe44",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "ad37ba767dd89d2ff6d7",
+    "url": "/homework/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "cb4b85839de55025742d",
+    "url": "/homework/static/css/main.07021b4f.chunk.css"
+  },
+  {
+    "revision": "ad37ba767dd89d2ff6d7",
+    "url": "/homework/static/js/2.411d2cfc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/homework/static/js/2.411d2cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4b85839de55025742d",
+    "url": "/homework/static/js/main.77564205.chunk.js"
+  },
+  {
+    "revision": "0d07d079105ea8862888",
+    "url": "/homework/static/js/runtime-main.7ce615df.js"
+  }
+]);
