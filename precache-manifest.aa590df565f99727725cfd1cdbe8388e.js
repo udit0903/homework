b1b1352c941ac8ea98b00ee55abe111dd2224826
udit0903 <63818954+udit0903@users.noreply.github.com>
2020-08-06 23:58:26 +0530

    Updates

---
+++ precache-manifest.aa590df565f99727725cfd1cdbe8388e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e93212fbcb4249d53305612e2210be3",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "7c367f7f164bd912288b",
+    "url": "/homework/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "6444fc070ecfbcce11e5",
+    "url": "/homework/static/css/main.07021b4f.chunk.css"
+  },
+  {
+    "revision": "7c367f7f164bd912288b",
+    "url": "/homework/static/js/2.c49eb974.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/homework/static/js/2.c49eb974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6444fc070ecfbcce11e5",
+    "url": "/homework/static/js/main.0eee415f.chunk.js"
+  },
+  {
+    "revision": "0d07d079105ea8862888",
+    "url": "/homework/static/js/runtime-main.7ce615df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/screen1/precache-manifest.443f319d1139980b38d178af1b8851b0.js"
+  "/homework/precache-manifest.aa590df565f99727725cfd1cdbe8388e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/screen1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homework/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
